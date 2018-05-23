86988be415a250d5b5fd61261aab95c5581060c6
Patrick Beam <patrick@mybeam.us>
2018-05-22 22:54:14 -0500

    remove chown command

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo chown -R jenkins:jenkins /home/jenkins/.ssh
+#sudo chown -R jenkins:jenkins /home/jenkins/.ssh
 
 exec java -jar /usr/share/jenkins/slave.jar -jnlpUrl $JENKINS_URL -secret $JENKINS_SECRET -workDir "/home/jenkins"
