ea896971dc92a094276c5dc0d666302a0a287e10
Patrick Beam <patrick@mybeam.us>
2018-05-22 22:04:13 -0500

    changing exec command again

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 chown -R jenkins:jenkins /home/jenkins/.ssh
 
-exec jenkins-slave -url $JENKINS_URL $JENKINS_SECRET $JENKINS_AGENT_NAME 
+exec java -jar /usr/share/jenkins/slave.jar -jnlpUrl $JENKINS_URL -secret $JENKINS_SECRET -workDir "/home/jenkins"
