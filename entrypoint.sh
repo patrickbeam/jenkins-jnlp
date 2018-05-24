91bfa36a15475ee1928929ed17bd609f99d19737
Patrick Beam <patrick@mybeam.us>
2018-05-24 17:31:08 -0500

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-if [ "$AVD" ]; then
-    socat tcp-listen:5555,bind=127.0.0.1,fork tcp:$AVD & SOCAT_PID=$!
-fi
-
-java -jar /slave.jar \
-    url $JENKINS_URL $JENKINS_SECRET
-
-if [ "$AVD_PID" ]; then
-    kill $SOCAT_PID
-fi
+java -jar /slave.jar -jnlpUrl $JENKINS_URL -secret $JENKINS_SECRET -workDir "/root"
